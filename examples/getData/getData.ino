d1165d08a6796324e13569c3350696f2a6f23d1e
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-09-02 16:34:09 +0800

    Update getData.ino

--- examples/getData/getData.ino
+++ examples/getData/getData.ino
@@ -10,24 +10,27 @@
 Bonezegei_Protocol bzp;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   //declared data (this is a preset data)
-  sprintf(bzp.data, "KILOGRAM1\3KILOGRAM5\3KILOGRAM9\3KILOGRAM54\3KILOGRAM123\3");
+  sprintf(bzp.data, "\1\3 Value1\3 Value2\3 value3\3 value 4\3 Bonezegei\3\2");
 
   //index array
-  const char *index[5] = { "K1", "K5", "K9", "54", "123" };
+  const char *index[5] = { "v1", "v2", "v3", "v4", "v5" };
   bzp.addIndex(index, 5);
 
   //display index array
-  for (int a = 0; a < bzp.index_size; a++) {
+  for (int a = 1; a <=bzp.size(); a++) {
     Serial.printf("INDEX:%s %d \n", bzp.index[a].name, bzp.index[a].index);
   }
 
   //display data from index
-  Serial.printf("DATA: %s \n", bzp.getData("54"));
-  Serial.printf("DATA: %s \n", bzp.getData("K1"));
+  Serial.printf("DATA: %s \n", bzp.getData("v1"));
+  Serial.printf("DATA: %s \n", bzp.getData("v2"));
+  Serial.printf("DATA: %s \n", bzp.getData("v5"));
+  Serial.printf("size: %d \n", bzp.size());
 }
 
 void loop() {
+
 }
