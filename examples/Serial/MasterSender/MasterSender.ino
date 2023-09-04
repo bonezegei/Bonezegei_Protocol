3de7481779c1a831feb0015c8d704ce011227c86
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-09-04 22:56:01 +0800

    Create MasterSender.ino

---
+++ examples/Serial/MasterSender/MasterSender.ino
@@ -0,0 +1,25 @@
+/*
+  Master Sender
+  Sample Data from Index
+  Author: Bonezegei (Jofel Batutay)
+  Date: August 2023 
+*/
+
+#include <Bonezegei_Protocol.h>
+
+Bonezegei_Protocol bzp;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  bzp.clear();
+  bzp.addData("millis");
+  bzp.addData((uint32_t)millis());
+  bzp.addData(8.5467);
+  bzp.endData();
+  Serial.printf("%s",bzp.rawData());
+  delay(30);
+}
+
